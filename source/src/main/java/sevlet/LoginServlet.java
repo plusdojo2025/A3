d04d2892bd2a750dc537cdb2ece12411001da8d1
plusdojo2025-Mori <mori-akane-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-12 15:10:24 +0900

    test

---
+++ source/src/main/java/sevlet/LoginServlet.java
@@ -0,0 +1,10 @@
+package sevlet;
+
+public class LoginServlet {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
--- source/src/main/java/sevlet/SimuServlet.java
+++ source/src/main/java/sevlet/SimuServlet.java
@@ -1,6 +1,6 @@
 package sevlet;
 
-public class Login {
+public class SimuServlet {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
