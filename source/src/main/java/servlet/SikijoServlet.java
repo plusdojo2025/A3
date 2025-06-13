cfb270f4a246bba4117eec07d306253f10bb7957
plusdojo2025-fujiwara <fujiwara-rio-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-13 15:44:16 +0900

    test

---
+++ source/src/main/java/servlet/SikijoServlet.java
@@ -0,0 +1,49 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class SikijoServlet
+ */
+@WebServlet("/SikijoServlet")
+public class SikijoServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SikijoServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+		// メニューページにフォワードする
+					RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/sikijo.jsp");  
+					// メニューページ（menu.jsp）へのディスパッチャを取得
+
+					dispatcher.forward(request, response);  
+					// メニューページへリクエストとレスポンスを転送
+	}
+
+}
