405882061464285ca9c313963c958914d0b34697
plusdojo2025-morioka <morioka-yuki-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-13 15:52:37 +0900

    test

--- source/src/main/java/servlet/CDataServlet.java
+++ source/src/main/java/servlet/CDataServlet.java
@@ -1,7 +1,10 @@
-package servlet;
+ package servlet;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -13,6 +16,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/CDataServlet")
 public class CDataServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private ServletRequest request;
        
     /**
      * @see HttpServlet#HttpServlet()
@@ -28,6 +32,11 @@ public class CDataServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/cdata.jsp");  
+		// メニューページ（menu.jsp）へのディスパッチャを取得
+
+		dispatcher.forward(request, response); 
 	}
 
 	/**
@@ -36,6 +45,12 @@ public class CDataServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
+		
+		
+		
 	}
+	
+	
+	
 
 }
