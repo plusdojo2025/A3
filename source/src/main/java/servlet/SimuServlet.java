e5facefa726c6eda947f8aaa25ac373e1a6afa02
plusdojo2025-Mori <mori-akane-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-16 10:31:00 +0900

    test

--- source/src/main/java/servlet/SimuServlet.java
+++ source/src/main/java/servlet/SimuServlet.java
@@ -28,8 +28,6 @@ public class SimuServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
 		// メニューページにフォワードする
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/simu.jsp");  
 		// メニューページ（menu.jsp）へのディスパッチャを取得
