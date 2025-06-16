1d000e8873a64c00b86a4bfae5754861a43964c6
plusdojo2025-ogawa <ogawa-kazuma-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-16 10:29:37 +0900

    text

--- source/src/main/java/servlet/LoginServlet.java
+++ source/src/main/java/servlet/LoginServlet.java
@@ -28,10 +28,7 @@ public class LoginServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	
-		// メニューページにフォワードする
+	// メニューページにフォワードする
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/login.jsp");  
 		// メニューページ（menu.jsp）へのディスパッチャを取得
 
--- source/src/main/java/servlet/PWelcomeServlet.java
+++ source/src/main/java/servlet/PWelcomeServlet.java
@@ -28,9 +28,6 @@ public class PWelcomeServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	
 		// メニューページにフォワードする
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/pwelcome.jsp");  
 		// メニューページ（menu.jsp）へのディスパッチャを取得
