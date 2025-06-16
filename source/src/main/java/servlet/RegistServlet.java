83a1d6d5459b370dd91b1944fb2ed34184cc6198
plusdojo2025-Mori <mori-akane-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-16 15:44:35 +0900

    test

--- source/src/main/java/servlet/RegistServlet.java
+++ source/src/main/java/servlet/RegistServlet.java
@@ -28,12 +28,15 @@ public class RegistServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		
 		// メニューページにフォワードする
-		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/regist.jsp");  
-		// メニューページ（menu.jsp）へのディスパッチャを取得
+					RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/regist.jsp");  
+					// メニューページ（menu.jsp）へのディスパッチャを取得
 
-		dispatcher.forward(request, response);  
-		// メニューページへリクエストとレスポンスを転送
+					dispatcher.forward(request, response);  
+					// メニューページへリクエストとレスポンスを転送
 	}
 
 	/**
@@ -41,7 +44,39 @@ public class RegistServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+		
 	}
 	
+	// リクエストパラメータを取得する
+			//request.setCharacterEncoding("UTF-8");
+			//String user_id = request.getParameter("user_id");
+			//String id = request.getParameter("id");
+			//String pw = request.getParameter("pw");
+			//String f_name = request.getParameter("f_name");
+			//String l_name = request.getParameter("l_name");
+			//String k_f_name = request.getParameter("k_f_name");
+			//String k_l_name = request.getParameter("k_l_name");
+			//String birthday = request.getParameter("birthday");
+			//String gender = request.getParameter("gender");
+			//String zipcode = request.getParameter("zipcode");
+			//String address = request.getParameter("address");
+			//String email = request.getParameter("email");
+			//String phone = request.getParameter("phone");
+
+
+			// 登録処理を行う
+			//UserDAO uDao = new UserDAO();
+			//if (uDao.insert(new User(0, company, department, position, name,
+					//zipcode, address, phone, fax, email, remarks))) { // 登録成功
+				//request.setAttribute("result", new Result("登録成功！", "レコードを登録しました。", "/webapp/RegistServlet"));
+			//} else { // 登録失敗
+				//request.setAttribute("registErr","レコードを登録できませんでした。");
+				//request.setAttribute("result", new Result("登録失敗！", "レコードを登録できませんでした。", "/webapp/RegistServlet"));
+			//}
+
+			// 結果ページにフォワードする
+			//RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/login.jsp");
+			//dispatcher.forward(request, response);
+		//}
+
 }
