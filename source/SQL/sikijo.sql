dba7a14a745918efcc8b38c576b62f7331814187
plusdojo2025-fujiwara <fujiwara-rio-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-17 15:45:41 +0900

    neko

--- source/SQL/sikijo.sql
+++ source/SQL/sikijo.sql
@@ -2,6 +2,6 @@ create table sikijo (
  sikijo_id int (30) primary key auto_increment,
  jm_number int (30) not null,
  name varchar (30) not null,
- address varchar (30) not null
+ address varchar (30) not null,
  image varchar (50)
 );
