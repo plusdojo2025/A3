25f0dbeeebc6a323f8f02fc9c425609245bdd458
plusdojo2025-fujiwara <fujiwara-rio-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-17 23:10:32 +0900

    neko

--- source/SQL/sc.sql
+++ source/SQL/sc.sql
@@ -1,5 +1,39 @@
+/*1つの式場に複数のコースがあることに気が付きました（6/17藤原）*/
+/*↓エラーになる*/
 create table sc (
  sc_id int  primary key auto_increment,
  sikijo_id int (30) not null,
  course_id int (30) not null
 );
+
+insert into sc value 
+(null,'1','2'),
+(null,'2','1,2,3'),
+(null,'3','2'),
+(null,'4','1'),
+(null,'5','1,2,3'),
+(null,'6','2'),
+(null,'7','1,2,3');
+/**/
+
+/*複数値をリレーションテーブルで処理できる
+ CREATE TABLE sc (
+  sc_id INT AUTO_INCREMENT PRIMARY KEY,
+  sikijo_id INT NOT NULL,
+  course_id INT NOT NULL
+);
+INSERT INTO sc (sc_id, sikijo_id, course_id) VALUES
+(NULL, 1, 2),
+(NULL, 2, 1),
+(NULL, 2, 2),
+(NULL, 2, 3),
+(NULL, 3, 2),
+(NULL, 4, 1),
+(NULL, 5, 1),
+(NULL, 5, 2),
+(NULL, 5, 3),
+(NULL, 6, 2),
+(NULL, 7, 1),
+(NULL, 7, 2),
+(NULL, 7, 3);
+ */
--- source/SQL/sp.sql
+++ source/SQL/sp.sql
@@ -3,3 +3,33 @@ create table sp (
  sikijo_id int (30) not null,
  planner_id int (20) not null
 );
+
+/*複数値をリレーションテーブルで処理できる
+ CREATE TABLE sp (
+  sp_id INT AUTO_INCREMENT PRIMARY KEY,
+  sikijo_id INT NOT NULL,
+  planner_id INT NOT NULL
+);
+INSERT INTO sp (sp_id, sikijo_id, planner_id) VALUES
+(NULL, 1, 1),
+(NULL, 1, 2),
+(NULL, 1, 3),
+(NULL, 2, 4),
+(NULL, 2, 5),
+(NULL, 2, 6),
+(NULL, 3, 7),
+(NULL, 3, 8),
+(NULL, 3, 9),
+(NULL, 4, 10),
+(NULL, 4, 11),
+(NULL, 4, 12),
+(NULL, 5, 13),
+(NULL, 5, 14),
+(NULL, 5, 15),
+(NULL, 6, 16),
+(NULL, 6, 17),
+(NULL, 6, 18),
+(NULL, 7, 19),
+(NULL, 7, 20),
+(NULL, 7, 21);
+ */
