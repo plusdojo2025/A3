32436f37fd86a46b91855e4c982501d25446555e
plusdojo2025-ogawa <ogawa-kazuma-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-18 09:44:18 +0900

    test

--- source/src/main/java/servlet/LoginServlet.java
+++ source/src/main/java/servlet/LoginServlet.java
@@ -16,7 +16,7 @@ import dto.AllDTO;
 /**
  * Servlet implementation class LoginServlet
  */
-@WebServlet("/LoginServlet")
+@WebServlet(urlPatterns = {"/","/LoginServlet"})
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
