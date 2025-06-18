17ba9c140b92194373c23d6f73738e6056d5b897
plusdojo2025-fujiwara <fujiwara-rio-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-18 10:53:22 +0900

    neko

--- source/SQL/sc.sql
+++ source/SQL/sc.sql
@@ -1,5 +1,5 @@
-/*1つの式場に複数のコースがあることに気が付きました（6/17藤原）*/
-/*↓エラーになる*/
+/*1つの式場に複数のコースがあることに気が付きました（6/17藤原）
+↓エラーになる
 create table sc (
  sc_id int  primary key auto_increment,
  sikijo_id int (30) not null,
@@ -14,9 +14,9 @@ insert into sc value
 (null,'5','1,2,3'),
 (null,'6','2'),
 (null,'7','1,2,3');
-/**/
+*/
 
-/*複数値をリレーションテーブルで処理できる
+/*複数値をリレーションテーブルで処理できる*/
  CREATE TABLE sc (
   sc_id INT AUTO_INCREMENT PRIMARY KEY,
   sikijo_id INT NOT NULL,
@@ -36,4 +36,3 @@ INSERT INTO sc (sc_id, sikijo_id, course_id) VALUES
 (NULL, 7, 1),
 (NULL, 7, 2),
 (NULL, 7, 3);
- */
