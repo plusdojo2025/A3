472e17de74948c847fe9353356d072c1c6ee38b4
plusdojo2025-kaneda <kaneda-ayami-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-18 13:48:14 +0900

    test

--- source/src/main/java/dao/ApplyDAO.java
+++ source/src/main/java/dao/ApplyDAO.java
@@ -1,5 +1,70 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import dto.ApplyDTO;
+
 public class ApplyDAO {
+	
+	public List<ApplyDTO> apply() {
+		List<ApplyDTO> applyList = new ArrayList<>();
+		Connection conn = null;
+		
+		try {
+			// JDBCドライバを読み込む
+			Class.forName("com.mysql.cj.jdbc.Driver");
+
+			// データベース(webapp2)に接続する
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/kkss?"
+					+ "characterEncoding=utf8&useSSL=false&serverTimezone=GMT%2B9&rewriteBatchedStatements=true",
+						"root", "password");
+			
+			//SQL文準備（applyデータ全て取得）
+			String sql = "SELECT * FROM apply";
+			PreparedStatement pStmt = conn.prepareStatement(sql);
+			
+			//SQL文実行
+			ResultSet rs = pStmt.executeQuery();
+			
+			while (rs.next()) {
+				ApplyDTO app = new ApplyDTO();
+				
+				app.setCourseId(rs.getInt("apply_id"));
+				app.setCourseId(rs.getInt("user_id"));
+				app.setCourseId(rs.getInt("sikijo_id"));
+				app.setCourseId(rs.getInt("planner_id"));
+				app.setCourseId(rs.getInt("course_id"));
+				
+				//applyListに表の1行目から結果を入れていく
+				applyList.add(app);
+			}
+			
+			
+		}catch (SQLException e) {
+			e.printStackTrace();
+			applyList = null;
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			applyList = null;
+		} finally {
+			// データベースを切断
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+					applyList = null;
+				}
+			}
+		}
+		// 結果を返す
+		return applyList;
+	}
 
 }
--- source/src/main/java/dto/ApplyDTO.java
+++ source/src/main/java/dto/ApplyDTO.java
@@ -1,5 +1,50 @@
 package dto;
 
-public class ApplyDTO {
+import java.io.Serializable;
 
+public class ApplyDTO implements Serializable {
+	private int applyId;
+	private int userId;
+	private int plannerId;
+	private int sikijoId;
+	private int courseId;
+	
+	
+	public int getApplyId() {
+		return applyId;
+	}
+	public void setApplyId(int applyId) {
+		this.applyId = applyId;
+	}
+	
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	
+	public int getPlannerId() {
+		return plannerId;
+	}
+	public void setPlannerId(int plannerId) {
+		this.plannerId = plannerId;
+	}
+	
+	public int getSikijoId() {
+		return sikijoId;
+	}
+	public void setSikijoId(int sikijoId) {
+		this.sikijoId = sikijoId;
+	}
+	
+	public int getCourseId() {
+		return courseId;
+	}
+	public void setCourseId(int courseId) {
+		this.courseId = courseId;
+	}
+	
+	
+	
 }
